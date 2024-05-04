01bf5d4de0b19f0feb82d647c9c09b2c6fcccf37
Mrhamza01 <hamza.kamboh035@gmail.com>
2024-05-04 20:55:30 +0500

    chagne the server link

--- src/lib/linkTokApi.ts
+++ src/lib/linkTokApi.ts
@@ -115,7 +115,7 @@ export const linkTokApi = createApi({
   
   reducerPath: "linkTokApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://127.0.0.1:8000/api/",
+    baseUrl: "http://localhost:8000/api/",
     credentials: "include",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token");
